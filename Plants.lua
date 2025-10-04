cef6a2fe773d2e91eeaacd0d7a068e8a4ee193e4
roiz <roizultra@gmail.com>
2025-10-03 21:49:45 -0300

    Update Plants.lua

--- Plants.lua
+++ Plants.lua
@@ -1,14 +1,19 @@
 return {
-    "Evo Mushroom III",
-    "Evo Mushroom II",
-    "Evo Mushroom I",
-    "Evo Blueberry III",
-    "Evo Blueberry II",
-    "Evo Blueberry I",
-    "Evo Beetroot III",
-    "Evo Beetroot II",
-    "Evo Beetroot I",
-    "Evo Pumpkin III",
-    "Evo Pumpkin II",
-    "Evo Pumpkin I",
+    Evoplants = { 
+        "Evo Mushroom III",
+        "Evo Mushroom II",
+        "Evo Mushroom I",
+
+        "Evo Blueberry III",
+        "Evo Blueberry II",
+        "Evo Blueberry I",
+
+        "Evo Beetroot III",
+        "Evo Beetroot II",
+        "Evo Beetroot I",
+
+        "Evo Pumpkin III",
+        "Evo Pumpkin II",
+        "Evo Pumpkin I"
+    }
 }
