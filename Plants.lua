d7116851a957744e513a42968d927f65f9b7960d
roiz <roizultra@gmail.com>
2025-10-16 18:23:48 -0300

    Halloween Event

--- Plants.lua
+++ Plants.lua
@@ -53,22 +53,13 @@ return {
     "Crimson Thorn",
     },
     
-    Evo = { 
-    "Evo Mushroom III",
-    "Evo Mushroom II",
-    "Evo Mushroom I",
-
-    "Evo Blueberry III",
-    "Evo Blueberry II",
-    "Evo Blueberry I",
-
-    "Evo Beetroot III",
-    "Evo Beetroot II",
-    "Evo Beetroot I",
-
-    "Evo Pumpkin III",
-    "Evo Pumpkin II",
-    "Evo Pumpkin I"
+    Event = { 
+    "Poison Apple",
+    "Seer Vine",
+    "Chicken Feed",
+    "Ghoul Root",
+    "Jack O Lantern",
+    "Bloodred Mushroom"
     },
 
 }
